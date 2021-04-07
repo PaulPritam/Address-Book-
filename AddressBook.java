9d10832d42e3805db0ec9b562bbb2c35c39bb721
unknown <pritampaul9798@gmail.com>
2021-04-08 01:44:55 +0530

    basic user inputs

--- AddressBook.java
+++ AddressBook.java
@@ -6,4 +6,20 @@ public class AddressBook {
    public static void main(String[] args )
    {
        System.out.println("welcome to address book ");
+       Scanner sc = new Scanner(System.in);
+       System.out.println("enter firstname");
+       String fname = sc.next();
+       System.out.println("enter lastname");
+       String lname = sc.next();
+       System.out.println("enter address");
+       String adr = sc.next();
+       System.out.println("enter state");
+       String state = sc.next();
+       System.out.println("enter city");
+       String city = sc.next();
+       System.out.println("enter zip");
+       String zip = sc.next();
+       System.out.println("enter phonenum");
+       String phno = sc.next();
    }
+}
