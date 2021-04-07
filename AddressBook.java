5d3d94087c8e9c579bc839091e015d0adec461d0
unknown <pritampaul9798@gmail.com>
2021-04-08 01:40:13 +0530

    added welcome file

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+   public static void main(String[] args )
+   {
+       System.out.println("welcome to address book ");
+   }
